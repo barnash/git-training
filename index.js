86885673d57ee5875e49c5fe37b9803cfd1ae8b2
Aviv Ben-Yosef <aviv.by@gmail.com>
2014-11-05 14:20:06 +0200

    Cosmetics

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function main() {
     var bodyEl = document.getElementsByTagName('body')[0];
     addIftachStuff(bodyEl);
     addAvivStuff(bodyEl);
-};
+}
 
 function addIftachStuff(bodyEl) {
     var d = document.createElement('h3');

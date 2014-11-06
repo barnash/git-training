f823498ca9c505193c224c8b6113d41b63ed684b
amir kalron <amir.kalron@gmail.com>
2014-11-06 13:27:46 +0200

    addd

---
+++ indexamirk.js
@@ -0,0 +1,13 @@
+
+function main() {
+    var bodyEl = document.getElementsByTagName('body')[0];
+    addIftachStuff(bodyEl);
+    addAvivStuff(bodyEl);
+    //addTomerStuff(bodyEl);
+    addAmirkStuff(bodyEl);
+}
+
+
+function addAmirkStuff(bodyEl) {
+    alert('Ducks')
+}

4560bf244b4c81bb25ce5ad27a0d0d282569485e
Tomer Mevorach <tomer@tomers-mbp.ravello.local>
2014-11-06 13:16:30 +0200

    sdfgsfgdsfgdfsg

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function main() {
     var bodyEl = document.getElementsByTagName('body')[0];
     addIftachStuff(bodyEl);
     addAvivStuff(bodyEl);
+    
     //addTomerStuff(bodyEl);
     addTamirStuff(bodyEl);
 }
